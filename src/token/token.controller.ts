1d2913721d5cbbb12a65a22dab022dd2e28a0b79
Vadim-Bykov <bvntaev@gmail.com>
2025-04-09 14:48:57 +0200

    fix: update node version

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Patch } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { TokenService } from './token.service';
 import { RefreshTokenDto } from './dto/create-token.dto';
+import { Public } from 'src/auth/auth.public.decorator';
 
 @ApiTags('Tokens')
 @Controller('token')
@@ -9,7 +10,8 @@ export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
   @Patch('refresh')
-  refreshToken(@Body() body: RefreshTokenDto) {
+  @Public()
+  async refreshToken(@Body() body: RefreshTokenDto) {
     return this.tokenService.refreshToken(body.token);
   }
 }
